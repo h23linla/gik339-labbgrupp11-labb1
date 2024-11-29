dcac9e8b058215f622717e9c902b97188b5c2527
h23linla <h23linla@du.se>
2024-11-29 10:49:35 +0100

    Hämtat/sparat till script

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 const theCheckbox = document.getElementsByClassName("sub-section_three");
+console.log(theCheckbox); /*Här kollar vi att vi hämtat upp input och sparat till variabeln*/
+
+const allTextFields = document.querySelectorAll(".textfield");
+console.log(allTextFields); /*Här kollar vi att vi hämtat upp alla textfält och sparat till variabeln*/
+
+const theBigButt = document.getElementById("changeSize");
+console.log(theBigButt); /*Här kollar vi att vi hämtat upp knappen och sparat till variabeln*/
+
+const theEmptyDiv = document.querySelector("#empty-div");
+console.log(theEmptyDiv); /*Här kollar vi att vi hämtat upp den tomma diven och sparat till variabeln*/
