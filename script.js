453e82c2f1e2191c67cce49750803b092b684b84
h23linla <h23linla@du.se>
2024-11-29 11:53:48 +0100

    Kopplat lyssnare, gjort funktioner

--- script.js
+++ script.js
@@ -9,3 +9,34 @@ console.log(theBigButt); /*Här kollar vi att vi hämtat upp knappen och sparat 
 
 const theEmptyDiv = document.querySelector("#empty-div");
 console.log(theEmptyDiv); /*Här kollar vi att vi hämtat upp den tomma diven och sparat till variabeln*/
+
+for(let i = 0; i <allTextFields.length; i++) {
+    allTextFields[i].addEventListener("click", handleClick);
+}
+
+allTextFields.forEach((field) => {
+    field.addEventListener("blur", handleBlur);
+});
+
+
+function handleBlur(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+    const html = `<p>Fältet ${name} har värdet ${value}</p>`;
+    theEmptyDiv.insertAdjacentHTML("beforeend", html);
+}
+
+function handleClick(e) {
+    e.preventDefault();
+    const targetName = e.target.id;
+
+    const colorField = settings.color;
+    const contentField = settings.content;
+
+    theEmptyDiv.style.backgroundColor = colorField.value;
+    theEmptyDiv.innerHTML = contentField.value;
+
+    theEmptyDiv.insertAdjacentElement("afterend", theBigButt);
+    console.log(targetName);
+    console.log("handleClick, eventobjekt: ", e);
+}
