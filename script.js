6ebfcf1f3ab127323a9ad9373e3c4850cae67dc6
h23linla <h23linla@du.se>
2024-12-02 10:16:16 +0100

    Vi har ändrat i nån funktion

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ console.log(theBigButt);
 const theEmptyDiv = document.querySelector("#empty-div");
 console.log(theEmptyDiv);
 
+const colorField = settings.color;
+
 /* ---------------------------------------------- */
 
 
@@ -17,17 +19,25 @@ for(let i = 0; i <allTextFields.length; i++) {
     allTextFields[i].addEventListener("blur", handleBlur);
 };
 
+for(let i = 0; i <theCheckbox)
+
 theCheckbox.forEach((field) => {
     field.addEventListener("change", (e) => {
         const value = e.target.value;
         console.log("Change value: ", value);
+        /*console.log(this.checked);*/
     });
     /*field.addEventListener("change", handleColor);*/
 }); /*vad är field?*/
 
-function handleColor(e) {
-    const value = e.target.value; /*value används aldrig här*/
-}
+theCheckbox.addEventListener("click", () => {
+    const selectedColor = colorField.value;
+    theEmptyDiv.style.backgroundColor = selectedColor;
+})
+
+/*function handleColor(e) {
+    const value = e.target.value;
+} */
 
 function handleBlur(e) {
     const name = e.target.name;
@@ -41,10 +51,10 @@ function handleClick(e) {
     e.preventDefault();
     const targetName = e.target.id; /*vilket ID hade det vi tryckte på?*/
 
-    const colorField = settings.color; /*settings=formulärets namn*/
+    /*const colorField = settings.color; settings=formulärets namn*/
     const contentField = settings.content;
 
-    theEmptyDiv.style.backgroundColor = colorField.value;
+    /*theEmptyDiv.style.backgroundColor = colorField.value;*/
     theEmptyDiv.innerHTML = contentField.value;
 
     theEmptyDiv.insertAdjacentElement("afterend", theBigButt); /*i.A.E vill ha något man skapat med .createElement eller hämtat med getelementbyid = HTMLElement-objekt. det är därför vi inte kunde lägga till andra grejer än theBigButt där*/
